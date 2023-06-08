8325b472a534f38a9811a31877a01c14d3dd686d
LightPride <vlad.grinevich1@gmail.com>
2023-06-08 17:44:19 +0300

    build check

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 
-export const App = () => {
-  return <div>React homework template</div>;
-};
+export default class App extends Component {
+  render() {
+    return;
+  }
+}
