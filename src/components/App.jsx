81fe27be9afca5111416f61abe400ee09d40a33c
LightPride <vlad.grinevich1@gmail.com>
2023-06-11 17:50:09 +0300

    remake

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,68 @@
 import { Component } from 'react';
 import SearchBar from './SearchBar/SearchBar';
 import ImageGallery from './ImageGallery/ImageGallery';
+import { getImages } from 'api';
+import Notiflix from 'notiflix';
+import Button from 'components/Button/Button';
 
 export default class App extends Component {
   state = {
     searchValue: '',
+    images: [],
+    page: 1,
+    error: null,
+    status: 'idle',
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const prevQuery = prevState.searchValue;
+    const nextQuery = this.state.searchValue;
+    const prevPage = prevState.page;
+    const nextPage = this.state.page;
+
+    if (prevQuery !== nextQuery || prevPage !== nextPage) {
+      this.fetchImages(nextQuery, nextPage);
+    }
+  }
+
+  fetchImages = async (nextQuery, nextPage) => {
+    await this.setState({ status: 'pending' });
+
+    try {
+      const data = await getImages({ nextQuery, nextPage });
+      if (data.hits.length === 0) {
+        Notiflix.Notify.failure("We're sorry, there are no matches found :(");
+        throw new Error("We're sorry, there are no matches found :(");
+      }
+      this.setState(prevState => ({
+        images: [...prevState.images, ...data.hits],
+        status: 'resolved',
+      }));
+    } catch (error) {
+      this.setState({ error, status: 'rejected' });
+    }
   };
 
   handleFormSubmit = searchValue => {
     this.setState({ searchValue });
   };
 
+  onLoadMore = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+  };
+
   render() {
     return (
       <>
         <SearchBar onSubmit={this.handleFormSubmit}></SearchBar>;
-        <ImageGallery searchValue={this.state.searchValue}></ImageGallery>;
+        <ImageGallery
+          images={this.state.images}
+          status={this.state.status}
+          error={this.state.error}
+        ></ImageGallery>
+        ;<Button onClick={this.onLoadMore}></Button>
       </>
     );
   }
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,83 +1,16 @@
 import { Component } from 'react';
 import { Gallery } from './ImageGallery.styled';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
-import Button from 'components/Button/Button';
+
 import Loader from 'components/Loader/Loader';
-import { getImages } from 'api';
-import Notiflix from 'notiflix';
 
 export default class ImageGallery extends Component {
-  state = {
-    images: null,
-    status: 'idle',
-    error: null,
-    currentPage: 1,
-  };
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.searchValue !== this.props.searchValue) {
-      this.fetchImages();
-    }
-    if (prevState.currentPage !== this.state.currentPage) {
-      this.fetchMoreImages();
-    }
-  }
-
-  fetchMoreImages = async () => {
-    const { searchValue } = this.props;
-    const { currentPage } = this.state;
-
-    try {
-      const data = await getImages({ searchValue, currentPage });
-
-      if (data.hits.length > 0) {
-        await this.setState(prevState => ({
-          images: {
-            ...prevState.images,
-            hits: [...(prevState.images?.hits || []), ...data.hits],
-          },
-          status: 'resolved',
-        }));
-      } else if (data.hits.length < 12) {
-        console.log(data.hits);
-        Notiflix.Notify.warning(
-          "We're sorry, but you've reached the end of search results."
-        );
-      }
-    } catch (error) {
-      this.setState({ error, status: 'rejected' });
-    }
-  };
-
-  fetchImages = async () => {
-    const { searchValue } = this.props;
-    const currentPage = 1;
-    await this.setState({ status: 'pending' });
-    try {
-      const data = await getImages({ searchValue, currentPage });
-
-      if (data.hits.length > 0) {
-        await this.setState({ images: data, status: 'resolved' });
-      } else if (data.hits.length === 0) {
-        Notiflix.Notify.failure("We're sorry, there are no matches found :(");
-        throw new Error("We're sorry, there are no matches found :(");
-      }
-    } catch (error) {
-      this.setState({ error, status: 'rejected' });
-    }
-  };
-
-  onLoadMore = () => {
-    this.setState(prevState => ({
-      currentPage: prevState.currentPage + 1,
-    }));
-  };
-
   render() {
+    const { images, status, error } = this.props;
     return (
       <>
-        {this.state.status === 'pending' && <Loader></Loader>}
-        {this.state.status === 'idle' && (
+        {status === 'pending' && <Loader></Loader>}
+        {status === 'idle' && (
           <h2
             style={{
               textAlign: 'center',
@@ -87,33 +20,29 @@ export default class ImageGallery extends Component {
             Lets find some pictures!
           </h2>
         )}
-        {this.state.status === 'resolved' && (
+        {status === 'resolved' && (
           <>
             <Gallery>
-              {this.state.images.hits.map(
-                ({ id, webformatURL, largeImageURL }) => {
-                  return (
-                    <ImageGalleryItem
-                      key={id}
-                      webImage={webformatURL}
-                    ></ImageGalleryItem>
-                  );
-                }
-              )}
+              {images.hits.map(({ id, webformatURL, largeImageURL }) => {
+                return (
+                  <ImageGalleryItem
+                    key={id}
+                    webImage={webformatURL}
+                  ></ImageGalleryItem>
+                );
+              })}
             </Gallery>
-
-            <Button onClick={this.onLoadMore}></Button>
           </>
         )}
 
-        {this.state.status === 'rejected' && (
+        {status === 'rejected' && (
           <h1
             style={{
               textAlign: 'center',
               marginTop: '50px',
             }}
           >
-            {this.state.error.message}
+            {error.message}
           </h1>
         )}
       </>
